654135abfe367da7e3aa57ddb15b823a5d1bba9d
dennis <denhouston1@icloud.com>
2022-10-14 13:31:20 +0100

    added props and video list length

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
     <SearchBar v-on:termChange="onTermChange" ></SearchBar>
+    <VideoList v-bind:videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import SearchBar from './components/SearchBar'
+  import VideoList from './components/VideoList'
+
   const API_KEY = 'AIzaSyBL3tstLEU1hNWoHXTXMQ4TXBKf-L1axMA';
 
   export default {
     name: 'App',
     components: {
-      SearchBar
+      SearchBar,
+      VideoList
+    },
+    data: function() {
+      return { videos: [] }
     },
     methods: {
       onTermChange: function(searchTerm){
-        console.log(searchTerm)
+        axios.get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        }).then(response => {
+          this.videos = response.data.items
+        })
       }
     }
   }
